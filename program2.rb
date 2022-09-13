d1bb0e5cd4873e9800e7f0e7901f77b56ff9afed
Ved Tiwari <ved@webkorps.com>
2022-09-14 01:12:14 +0530

    created array program how many items are grater then 5

---
+++ program2.rb
@@ -0,0 +1,12 @@
+def count(num , array)
+    cnt = 0
+    for item in array
+        if item > num
+            cnt+=1
+        end
+    end
+    return "Here #{cnt} items are grater then #{num}"
+end
+array= [2,4,8,45,2,19,91,0]
+num=5
+puts count(num,array)
