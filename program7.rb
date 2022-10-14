ebe6a4edeb9287f858f6b84c96fa9791dc0ef351
Ved Tiwari <ved@webkorps.com>
2022-10-15 01:40:15 +0530

    1 to 10 table program

--- program7.rb
+++ program7.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-p 'enter the number' #WAP to find even, odd, negetive, zero
+p 'enter the number' # WAP to find even, odd, negetive, zero
 num = Integer(gets.chomp)
 if num.negative?
   puts 'negetive'
---
+++ program8.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+    
+(1..10).each do |i|
+  (1..10).each do |j|
+    puts "#{i}*#{j} = #{i * j}"
+  end
+  puts '----------------------'
+end
