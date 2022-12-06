2040491182cd60932be4b7b5e3dcc03013b4567b
Ved Tiwari <ved@webkorps.com>
2022-12-06 20:22:09 +0530

    about Block in details

---
+++ blocks.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# pass one parameter in yield
+def test
+  yield 5
+  puts 'You are in the method'
+  yield 6
+end
+test { |f| puts "you are in the blook #{f}" }
+
+# more then one parameter in yield
+def test
+  yield 5, 9
+  puts 'You are in the method'
+  yield 6, 98
+end
+test { |a, b| puts "you are in the blook #{a} and #{b}" }
+
+# call block using block.call function
+def test(&block)
+  block.call
+end
+test { puts 'in the blook' }
+
+# using BEGIN and END block
+BEGIN {
+    puts 'BEGIN code block'
+}
+END {
+  puts 'END code block'
+}
+puts 'main block'
